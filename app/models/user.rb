b83ec4c6c30cc6b9e5159b425a6aff480cc7c8f8
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-13 13:23:31 -0500

    add has_secure_password to User model, for bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    # https://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html#method-i-has_secure_password
+    # comes from ActiveModel and adds methods to set and authenticate against a BCrypt password
+    has_secure_password
 end
