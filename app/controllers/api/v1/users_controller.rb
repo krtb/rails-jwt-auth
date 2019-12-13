563e2e2053e5f57ae3d734ccab289a8fccd51a6a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-13 14:54:21 -0500

    add create method to User class

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::UsersController < ApplicationController
+
+
+    def create
+    @user = User.create(user_params)
+    if @user.valid?
+      # https://gist.github.com/mlanett/a31c340b132ddefa9cca = send Rails HTTP status codes
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :bio, :avatar)
+  end
 end
