ee1294ea4e97a74c63bdc1d680619b8a192c1aa4
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-13 16:28:07 -0500

    add methods to check if user logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,23 @@ class ApplicationController < ActionController::API
     end
 
     def auth_header
-    # { 'Authorization': 'Bearer <token>' }
-    request.headers['Authorization']
-  end
+        # { 'Authorization': 'Bearer <token>' }
+        request.headers['Authorization']
+    end
+
+    def current_user
+        # Recall that a Ruby object/instance is 'truthy': !!user_instance #=> true and nil is 'falsey': !!nil #=> false
+
+        if decoded_token
+        # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
+        # or nil if we can't decode the token
+        user_id = decoded_token[0]['user_id']
+        @user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        # returns truthy or falsey, dependent on return value of current_user
+        !!current_user
+    end
 end
