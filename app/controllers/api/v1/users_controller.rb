c4bb5f85999cd7c751c6c459c4eb3f686e092c17
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-13 16:33:19 -0500

    modify UsersController to issue token on signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,22 @@
 class Api::V1::UsersController < ApplicationController
+skip_before_action :authorized, only: [:create]
 
-
-    def create
+  def create
     @user = User.create(user_params)
     if @user.valid?
+      #  issues a token when users register for our app
+      @token = encode_token(user_id: @user.id)
       # https://gist.github.com/mlanett/a31c340b132ddefa9cca = send Rails HTTP status codes
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :password, :bio, :avatar)
   end
+
 end
