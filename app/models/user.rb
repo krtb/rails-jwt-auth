8303efa7d428d4551a467d97b0a577499f983d08
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-13 13:25:36 -0500

    add validations to User class, username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     # https://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html#method-i-has_secure_password
     # comes from ActiveModel and adds methods to set and authenticate against a BCrypt password
     has_secure_password
+
+    # add validations for User class
+    validates :username, uniqueness: { case_sensitive: false }
 end
